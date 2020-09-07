385e20ebf9e4661c91d0cef8a79c2ca0dad0af17
AokoMercyline <aokomercyline34@gmail.com>
2020-09-08 01:47:13 +0300

    imported pyperclip and updated user function

--- user_credential.py
+++ user_credential.py
@@ -1,34 +1,37 @@
 import random
 import string
+import pyperclip
 
 class User:
+    
     user_list = []
     
-    def _init_(self,username,password):
+    def __init__(self,username,password):
         self.username = username
         self.password = password
         
     def save_user(self):
         User.user_list.append(self)
         
-class Credentials:
-    credentials_list = []
-    
     @classmethod
     
-    def user_exist(cls,username,password):
+    def user_exist(cls, username,password):
         current_user = ''
         for user in User.user_list:
             if(user.username == username and user.password == password):
-                current_user == user.username
+                current_user = user.username
         return current_user
+        
+class Credentials:
     
-    def _init_(self,account_name,username,password):
+    credentials_list = []
+    
+    def __init__(self,account_name,username, password):
         self.account_name = account_name
         self.username = username
         self.password = password
-        
-        
+    
+    
     def save_credentials(self):
         Credentials.credentials_list.append(self)
         
@@ -41,11 +44,10 @@ class Credentials:
         for credentials in cls.credentials_list:
             if credentials.account_name == account_name:
                 return credentials
-    
-    
+            
     @classmethod
     
-    def credentials_exist(cls,account_name):
+    def credentials_exist(cls, account_name):
         for credentials in cls.credentials_list:
             if credentials.account_name == account_name:
                 return True
@@ -56,10 +58,16 @@ class Credentials:
     def display_credentials(cls):
         return cls.credentials_list
     
+    @classmethod
+    
+    def copy_credentials(cls):
+        found_credentials = Credentials.find_account_name(account_name)
+        pyperclip.copy(found_credentials)
     
     def generate_password(stringLength = 8):
         password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "(|/~!.@,)#{?$[%]^}&*"
         return ''.join(random.choice(password) for i in range(stringLength))
+
     
         
         
